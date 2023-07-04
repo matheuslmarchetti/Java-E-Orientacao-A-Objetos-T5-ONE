706a2790bde9cecaf81376294bdb93d3f91cae20
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-07-04 14:50:45 -0400

    Aprendendo sintaxe Java

---
+++ sintaxevariaveisefluxo/src/sintaxevariaveisefluxo/TestaVariaveis.java
@@ -0,0 +1,14 @@
+package sintaxevariaveisefluxo;
+
+public class TestaVariaveis {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Olá, novo teste!");
+		
+		int idade;
+		idade = 37;
+		
+		System.out.println("A idade é " + idade + ", parabéns!");
+	}
+}
