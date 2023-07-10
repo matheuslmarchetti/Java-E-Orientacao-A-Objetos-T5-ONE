259ff48f02335cf85b75fc0c27886566d7d73cd1
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-07-10 13:33:22 -0400

    Aprendendo relacionamento de isntancia.

---
+++ bytebank/src/bytebank/TesteInstancia.java
@@ -0,0 +1,14 @@
+package bytebank;
+
+public class TesteInstancia {
+	public static void main(String[] args) {
+		Conta contaDaMarcela = new Conta();
+
+		contaDaMarcela.titular = new Cliente();
+		
+		contaDaMarcela.titular.nome = "Marcela";
+		
+		System.out.println(contaDaMarcela.titular);
+		System.out.println(contaDaMarcela.titular.nome);
+	}
+}
