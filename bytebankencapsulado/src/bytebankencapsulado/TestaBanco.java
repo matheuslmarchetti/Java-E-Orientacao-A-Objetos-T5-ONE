237d08e69da99820809b3d91bc3377538f0e97b3
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-07-11 13:52:18 -0400

    Aprendendo encapsulamento.

--- bytebankencapsulado/src/bytebankencapsulado/TestaBanco.java
+++ bytebankencapsulado/src/bytebankencapsulado/TestaBanco.java
@@ -15,8 +15,18 @@ public class TestaBanco {
 		System.out.println(contaDoPaulo.getTitular().getNome());
 		System.out.println(contaDoPaulo.getTitular());
 		
+		Cliente matheus = new Cliente();
+		matheus.setNome("Matheus Marchetti");
+		matheus.setCpf("244.244.555.-22");
+		matheus.setProfissao("desenvolvedor");
+		
 		Conta contaDoMatheus = new Conta(123,4567);
 		System.out.println(Conta.getTotal());
+		contaDoMatheus.deposita(100);
+
+		contaDoMatheus.setTitular(matheus);
+		System.out.println(contaDoMatheus.getTitular().getNome());
+		System.out.println(contaDoMatheus.getTitular());
 
 	}
 }
