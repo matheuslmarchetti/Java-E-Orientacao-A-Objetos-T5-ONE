fe611bdc805b65415723e9f7c0d6462d7383def6
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-07-12 10:50:16 -0400

    Criando projeto bytebankherdadoconta

--- bytebankherdadoconta/src/bytebankherdadoconta/ContaCorrente.java
+++ bytebankherdadoconta/src/bytebankherdadoconta/ContaCorrente.java
@@ -1,5 +1,14 @@
 package bytebankherdadoconta;
 
 public class ContaCorrente extends Conta {
-
+	
+	public ContaCorrente(int agencia, int numero){
+	    super(agencia, numero);
+	}
+	
+	@Override
+	public boolean saca(double valor) {
+		double valorASacar = valor + 0.2;
+		return super.saca(valorASacar);
+	}
 }
