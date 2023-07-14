95845d6f361faaabdc09d8416856d7aa27b21991
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-07-14 11:01:21 -0400

    Praticando herença e interfaces.

--- bytebankherdado/src/bytebankherdado/Administrador.java
+++ bytebankherdado/src/bytebankherdado/Administrador.java
@@ -1,26 +1,26 @@
 package bytebankherdado;
 
 public class Administrador extends Funcionario implements Autenticavel {
-	
-	private int senha;
-	
+		
 	@Override
 	public double getBonificacao() {
 		return 500;
 	}
 		
+	private AutenticacaoUtil autenticador;
+	
+	public Administrador() {
+		this.autenticador = new AutenticacaoUtil();
+	}
+
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;		
+		this.autenticador.setSenha(senha);
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if(this.senha == senha){
-            return true;
-        } else {
-            return false;
-        }
+		return this.autenticador.autentica(senha);
 	}
 	
 }
--- bytebankherdado/src/bytebankherdado/Gerente.java
+++ bytebankherdado/src/bytebankherdado/Gerente.java
@@ -1,25 +1,25 @@
 package bytebankherdado;
 
 public class Gerente extends Funcionario implements Autenticavel {
-	
-	private int senha;
-	
+		
 	public double getBonificacao() {
 		return getSalario() * 0.1;
 	}
 		
+	private AutenticacaoUtil autenticador;
+	
+	public Gerente() {
+		this.autenticador = new AutenticacaoUtil();
+	}
+
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;		
+		this.autenticador.setSenha(senha);
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if(this.senha == senha){
-            return true;
-        } else {
-            return false;
-        }
+		return this.autenticador.autentica(senha);
 	}
 	
 }
