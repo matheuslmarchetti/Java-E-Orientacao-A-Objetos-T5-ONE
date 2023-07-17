604467b8328e9fea9cc64bc3b0afd50353c43cea
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-07-17 19:15:55 -0400

    jar

--- bytebankbiblioteca/src/br/com/alura/bytebank/TesteBiblioteca.java
+++ bytebankbiblioteca/src/br/com/alura/bytebank/TesteBiblioteca.java
@@ -1,13 +1,15 @@
 package br.com.alura.bytebank;
 
-//import br.com.bytebank.banco.modelo.Conta;
-//import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
 
 public class TesteBiblioteca {
 
 	public static void main(String[] args) {
 		
-//		Conta c = new ContaCorrente(123, 321);
+		Conta c = new ContaCorrente(123, 321);
+		c.deposita(700.0);
+		System.out.println(c.getSaldo());
 
 	}
 
