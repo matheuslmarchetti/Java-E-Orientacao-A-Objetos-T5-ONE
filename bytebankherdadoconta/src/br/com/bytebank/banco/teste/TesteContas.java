328f01dbc6c1e3f9a244976ee01794f96352c14e
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-07-17 10:53:33 -0400

    Todos os modificadores de acesso

--- bytebankherdadoconta/src/br/com/bytebank/banco/teste/TesteContas.java
+++ bytebankherdadoconta/src/br/com/bytebank/banco/teste/TesteContas.java
@@ -1,11 +1,15 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.especial.ContaEspecial;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 import br.com.bytebank.banco.modelo.SaldoInsuficienteException;
 
 public class TesteContas {
 	public static void main(String[] args) throws SaldoInsuficienteException {
+		
+		ContaEspecial ce = new ContaEspecial(333, 333);
+		ce.deposita(300.00);
 		
 		ContaCorrente cc = new ContaCorrente(111,222);
 		cc.deposita(100.00);
@@ -17,5 +21,6 @@ public class TesteContas {
 		
 		System.out.println("CC: " + cc.getSaldo());
 		System.out.println("CP: " + cp.getSaldo());
+		System.out.println("CE: " + ce.getSaldo());
 	}
 }
