1551493b6d98358b34b2094bacc195b574c14afd
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-07-21 16:21:59 -0400

    Adicionando tela inicial

--- conversordecomprimento/src/br/com/one/alura/live/conversordecomprimento/modelo/ConversorGrafico.java
+++ conversordecomprimento/src/br/com/one/alura/live/conversordecomprimento/modelo/ConversorGrafico.java
@@ -1,9 +1,13 @@
 package br.com.one.alura.live.conversordecomprimento.modelo;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -18,16 +22,97 @@ public class ConversorGrafico extends JFrame {
 	private JMenuItem opcao2;
 	private JMenuItem opcao3;
 	
+	
 	public ConversorGrafico() {
+		setVisible(true);
 		setSize(600, 600);
 		setTitle("Conversor de Comprimentos");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setLayout(null);
 		
-		JPanel painel = new JPanel();
+		JLabel labelBemVindo = new JLabel("Bem-vindo(a) ao Conversor de Comprimentos");
+		labelBemVindo.setBounds(100, 100, 500, 70);
+		labelBemVindo.setFont(new Font("Arial", Font.BOLD, 16));
+		add(labelBemVindo);
 		
-		exibirMenu();
-		painel.add(menuBar);
-		add(painel);
+		final JButton buttonIniciar = new JButton("Iniciar");
+		buttonIniciar.setBounds(240, 300, 120, 30);
+		buttonIniciar.setFont(new Font("Arial", Font.BOLD, 20));
+		buttonIniciar.setFocusPainted(false);
+		buttonIniciar.setForeground(Color.WHITE);
+		buttonIniciar.setBackground(Color.BLACK);
+		buttonIniciar.setBorderPainted(false);
+
+		buttonIniciar.addMouseListener(new java.awt.event.MouseAdapter() {
+		    public void mouseEntered(java.awt.event.MouseEvent evt) {
+		    	buttonIniciar.setForeground(Color.BLACK);
+		    	buttonIniciar.setBackground(Color.GRAY);
+		    }
+
+		    public void mouseExited(java.awt.event.MouseEvent evt) {
+		    	buttonIniciar.setForeground(Color.WHITE);
+		    	buttonIniciar.setBackground(Color.BLACK);
+		    }
+		});
+		
+		buttonIniciar.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				dispose();                //close old frame
+	            JFrame painel = new JFrame("Conversor de Comprimentos");    //creat new frame
+	            painel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	            painel.add(new JPanel());      //add new panel
+	            painel.setVisible(true);       //make it visible
+	            painel.setSize(600,600);      //set the size
+	            painel.setLocationRelativeTo(null);// center frame
+	            painel.setLayout(null);
+	            
+	            
+	            
+//				JPanel painel = new JPanel();
+//				painel.setVisible(true);
+//				
+//				
+//				exibirMenu();
+//				painel.add(menuBar);
+//				add(painel);
+				
+			}
+		});
+		
+		final JButton buttonSair = new JButton("Sair");
+		buttonSair.setBounds(240, 350, 120, 30);
+		buttonSair.setFont(new Font("Arial", Font.BOLD, 20));
+		buttonSair.setFocusPainted(false);
+		buttonSair.setForeground(Color.WHITE);
+		buttonSair.setBackground(Color.BLACK);
+		buttonSair.setBorderPainted(false);
+
+		buttonSair.addMouseListener(new java.awt.event.MouseAdapter() {
+		    public void mouseEntered(java.awt.event.MouseEvent evt) {
+		    	buttonSair.setForeground(Color.BLACK);
+		    	buttonSair.setBackground(Color.GRAY);
+		    }
+
+		    public void mouseExited(java.awt.event.MouseEvent evt) {
+		    	buttonSair.setForeground(Color.WHITE);
+		    	buttonSair.setBackground(Color.BLACK);
+		    }
+		});
+		
+		buttonSair.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Saindo do programa...", "Mensagem", 
+				JOptionPane.WARNING_MESSAGE);
+				System.exit(0);
+				
+			}
+		});
+		
+		add(buttonIniciar);
+		add(buttonSair);
 		
 	}
 	
@@ -43,6 +128,14 @@ public class ConversorGrafico extends JFrame {
 		menu.add(opcao3);
 		
 		menuBar.add(menu);
+		
+		opcao1.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
 		
 		opcao3.addActionListener(new ActionListener() {
 			
--- conversordecomprimento/src/br/com/one/alura/live/conversordecomprimento/teste/TesteConversorGrafico.java
+++ conversordecomprimento/src/br/com/one/alura/live/conversordecomprimento/teste/TesteConversorGrafico.java
@@ -5,8 +5,7 @@ import br.com.one.alura.live.conversordecomprimento.modelo.ConversorGrafico;
 public class TesteConversorGrafico {
 	public static void main(String[] args) {
 	
-		ConversorGrafico janela = new ConversorGrafico();
-		janela.setVisible(true);
+		new ConversorGrafico();
 
 	}
 
